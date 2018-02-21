2a6ebf545a30047c2855f61228ee6cb3ac5b3679
Robert Rozas Navarro <robert.rozas.n@gmail.com>
2018-02-21 12:59:23 -0300

    se añaden demo sqlinjection solo falta mostrar

--- Seguridad.Net/Controllers/SecureController.cs
+++ Seguridad.Net/Controllers/SecureController.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Security.Application;
 using Newtonsoft.Json;
+using System.Configuration;
 using System.Data;
 using System.Data.SQLite;
 using System.Web.Mvc;
@@ -11,6 +12,7 @@ namespace Seguridad.Net.Controllers
 
     public class SecureController : Controller
     {
+        private static string liteCon = ConfigurationManager.ConnectionStrings["strCon"].ConnectionString;
         // GET: Secure
         // http://localhost:62921/Secure/TestXSS?Nombre=Robert Rozas <script>alert("XSS")</script>  Error
         // http://localhost:62921/Secure/TestXSS?Nombre=Robert Rozas \x3cscript\x3ealert(\x27doingSomethingNaughty\x27)\x3c/script\x3e  Bypass
@@ -31,7 +33,7 @@ namespace Seguridad.Net.Controllers
         // http://localhost:62921/Secure/TestSQLInjection?Nombre='or '1' = '1
         public ActionResult TestSQLInjection(string Nombre)
         {
-            var m_dbConnection = new SQLiteConnection("Data Source=BaseDatosSegura.sqlite;Version=3;");
+            var m_dbConnection = new SQLiteConnection(liteCon);
             m_dbConnection.Open();
             var tb = new DataTable();
             string sql = "select * from usuarios where nombre = @nombre" ;
